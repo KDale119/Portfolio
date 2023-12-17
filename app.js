f8d270c40b6ac13e3dffbb9272a74dee8a91d62e
Kayla Dale <kmeyers119@gmail.com>
2023-12-17 14:31:15 -0600

    BUTTONS FOR BLOG FUNCTIONALITY & STYLING COMPLETE

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function someThing() {
        // document.getElementById('home').innerHTML = result.navbarOpts[0].opt1
        for (let opt of result.navbarOpts) {
         let link = document.createElement('a')
+        
         link.setAttribute('href', opt.link)
         let h3 = document.createElement('h3')
         h3.className = opt.opt1
@@ -18,7 +19,6 @@ function someThing() {
         document.getElementById('navbarOpts').appendChild(link);
         link.appendChild(h3);
 
-        h3.id = 'home';
 
         
         }//         NAVBAR
@@ -66,7 +66,7 @@ function someThing() {
         let headr = document.getElementById('head');
         for(let head of result.Header) {
             let h4 = document.createElement("h4")
-            h4.id = 'experience';
+            
             h4.innerHTML = head.heading;
             headr.appendChild(h4);
         }// experience heading
@@ -120,13 +120,14 @@ function someThing() {
             imgDiv.appendChild(img);
         } //coding journey IMG- works needs styling
 
-        let btnsDiv = document.getElementById("buttons");
-       for(let btns of result.buttons){
-        let btn = document.createElement('button')
-        btn.className = "btns";
-        btn.innerHTML = btns.title;
-        btnsDiv.appendChild(btn);
-       }//blog buttons- need styling cant figure out
+    //     let btnsDiv = document.getElementById("buttons");
+    //    for(let btns of result.buttons){
+    //     let btn = document.createElement('button')
+    //     btn.className = "btns";
+    //     btn.id = btns.id
+    //     btn.innerHTML = btns.title;
+    //     btnsDiv.appendChild(btn);
+    //    }//blog buttons- need styling cant figure out
        
        let pDiv = document.getElementById('div')
        for(let p of result.blog){
@@ -267,4 +268,109 @@ function leftArrow(){
         }
     )}
 }
+
+// let journey = document.getElementById('journey');
+// function journeyBtn() {
+//     fetch("./page3.json")
+//     .then(response => response.json())
+//     .then(result => {
+//         let img = document.getElementById('img');
+//         img.setAttribute('src', results.buttons[0].img)
+            
+        
+//         let paragraph = document.getElementById('blogP')
+//         paragraph.innerHTML = result.buttons[0].desc;
+//     })
+// }
+// if(journey) {
+//     journey.addEventListener("click", journeyBtn())
+// }
+
+//                                 EVENT LISTENERS FOR BLOG BUTTONS 
+let journey = document.getElementById('journey');
+function journeyBtn() {
+    fetch("./page3.json")
+    .then(response => response.json())
+    .then(result => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', result.buttons[0].img)
+        let paragraph = document.getElementById('blogP')
+        paragraph.innerHTML = result.buttons[0].desc;    
+    })
+}
+let coffee = document.getElementById('coffee');
+function coffeeBtn() {
+    fetch("./page3.json")
+    .then(response => response.json())
+    .then(result => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', result.buttons[1].img)
+        let paragraph = document.getElementById('blogP')
+        paragraph.innerHTML = result.buttons[1].desc;    
+    })
+}
+let plants = document.getElementById('plants');
+function plantsBtn() {
+    fetch("./page3.json")
+    .then(response => response.json())
+    .then(result => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', result.buttons[2].img)
+        let paragraph = document.getElementById('blogP')
+        paragraph.innerHTML = result.buttons[2].desc;    
+    })
+}
+let learning = document.getElementById('learning');
+function learningBtn() {
+    fetch("./page3.json")
+    .then(response => response.json())
+    .then(result => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', result.buttons[3].img)
+        let paragraph = document.getElementById('blogP')
+        paragraph.innerHTML = result.buttons[3].desc;    
+    })
+}
+let reading = document.getElementById('reading');
+function readingBtn() {
+    fetch("./page3.json")
+    .then(response => response.json())
+    .then(result => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', result.buttons[4].img)
+        let paragraph = document.getElementById('blogP')
+        paragraph.innerHTML = result.buttons[4].desc;    
+    })
+}
+let wfh = document.getElementById('wfh');
+function wfhBtn() {
+    fetch("./page3.json")
+    .then(response => response.json())
+    .then(result => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', result.buttons[5].img)
+        let paragraph = document.getElementById('blogP')
+        paragraph.innerHTML = result.buttons[5].desc;    
+    })
+}
+
+
+
+
+
+
+
+// let journey = document.getElementById('journey');
+// journey.addEventListener('click', function(){
+//     fetch("./page3.json")
+//     .then(response => response.json())
+//     .then(result => {
+//         let img = document.getElementById('img');
+//         img.setAttribute('src', results.buttons[0].img)
+            
+        
+//         let paragraph = document.getElementById('blogP')
+//         paragraph.innerHTML = result.buttons[0].desc;
+//     })
+// });
 
